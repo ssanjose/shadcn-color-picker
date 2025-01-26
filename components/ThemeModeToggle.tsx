15ce884325e19ee40e439d33540b3ff10f64c5d9
ssanjose <ssanjose@academic.rrc.ca>
2025-01-26 10:15:04 -0600

    create: theme provider and toggle

---
+++ components/ThemeModeToggle.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import * as React from "react"
+import { Moon, Sun } from "lucide-react"
+import { useTheme } from "next-themes"
+
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { cn } from "@/lib/utils";
+
+const ThemeModeToggle = ({ className }: React.HTMLAttributes<HTMLButtonElement>) => {
+  const { setTheme } = useTheme()
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon" className={cn(`shadow-none relative top-0 right-0 size-8 w-fit h-fit p-2`, className)}>
+          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => setTheme("light")}>
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("dark")}>
+          Dark
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("system")}>
+          System
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
+
+export { ThemeModeToggle };
---
+++ components/ThemeProvider.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import dynamic from 'next/dynamic';
+import React from 'react';
+import { ThemeProvider as StaticProvider } from 'next-themes';
+
+// dynamic import
+// see https://github.com/shadcn-ui/ui/issues/5552#issuecomment-2435053678
+const DynProvider = dynamic(
+  () => import('next-themes').then((e) => e.ThemeProvider),
+  { ssr: false }
+);
+
+export function ThemeProvider({ children, ...props }: React.ComponentProps<typeof StaticProvider>) {
+  // const NextThemeProvider = process.env.NEXT_PUBLIC_NODE_ENV === 'production' ? StaticProvider : DynProvider;
+  const NextThemeProvider = StaticProvider;
+
+  return (
+    <NextThemeProvider {...props}>
+      {children}
+    </NextThemeProvider>
+  );
+}
+
+export default ThemeProvider;
