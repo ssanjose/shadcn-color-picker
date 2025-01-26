15675432d04c5871e8ecec4d55f7945e0dbc7e45
ssanjose <ssanjose@academic.rrc.ca>
2025-01-26 12:04:27 -0600

    update: page to add header and installation

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,165 @@
-import Image from "next/image";
+'use client';
+
+import React, { useEffect } from "react";
+import { siteConfig } from "@/config/site";
+import { ColorPicker, ColorPickerHex, ColorPickerInput } from "@/components/ui/color-picker";
+import { ColorContext, useColorContext } from "@/hooks/use-color-context";
+import { cn } from "@/lib/utils";
+import Link from "next/link"
+
+import { Inter } from 'next/font/google';
+import { Button } from "@/components/ui/button";
+import { MoveRight } from "lucide-react";
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [color, setColor] = React.useState("#FFFFFF");
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+  return (
+    <ColorContext.Provider value={color}>
+      <App color={color} setColor={setColor} />
+    </ColorContext.Provider>
+  );
+}
+
+const App = ({ color, setColor }: {
+  color: string;
+  setColor: React.Dispatch<React.SetStateAction<string>>;
+}) => {
+  const colorContext = useColorContext();
+  return (
+    <div style={{ backgroundColor: `${colorContext}` }} className={cn("min-h-screen py-[2.5rem]", inter.className)}>
+      <div className={cn(`container mx-auto max-w-[45rem] min-h-lvw space-y-4 bg-white p-4 py-12 rounded-lg bg-white/95 backdrop-blur supports-[backdrop-filter]:bg-white/90`)}>
+        <header className="space-y-2 pb-[5rem] flex flex-col items-center">
+          <div className="space-y-2 text-center mb-2">
+            <h1 className="text-3xl sm:text-5xl font-bold pt-2">{siteConfig.name}</h1>
+            <p className="text-sm sm:text-base text-muted-foreground max-w-[30rem]">
+              {siteConfig.description}
+            </p>
+            <div>
+              <Button asChild>
+                <Link href="#color-picker" className="text-muted-foreground">
+                  <span>Try it Out!</span>
+                </Link>
+              </Button>
+            </div>
+            <Button
+              variant="link"
+              asChild
+              className="hover:font-semibold"
+            >
+              <Link href={siteConfig.links.github}>
+                <span>Star on GitHub</span>
+                <MoveRight className="ml-2 h-4 w-4" />
+              </Link>
+            </Button>
+          </div>
+          <ColorPicker className="w-1/5 min-w-[10rem] mx-auto bg-white border-2 border-gray-300 rounded-xl" id="color-picker">
+            <ColorPickerHex
+              color={color}
+              onChange={setColor}
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+            <ColorPickerInput
+              type="text"
+              value={color}
+              onChange={(e) => setColor(e.target.value)}
+              placeholder="#000"
+            />
+          </ColorPicker>
+        </header>
+        <main>
+          <Installation />
+        </main>
+        <footer>
+
+        </footer>
+      </div>
     </div>
   );
 }
+
+const Installation = () => {
+  return (
+    <div className="space-y-8">
+      <h2 className="text-2xl font-bold mb-4">Installation</h2>
+      <div className="space-y-2">
+        <p className="text-muted-foreground">
+          Install react-colorful from npm.
+        </p>
+        <pre className="bg-gray-200 p-2 rounded-lg">
+          <code className="text-sm">
+            npm install react-colorful
+          </code>
+        </pre>
+      </div>
+      <div className="space-y-2">
+        <p className="text-accent-foreground">Copy the <a href="#color-picker" className="p-1 bg-muted rounded">Color Picker</a> component.</p>
+        <pre className="bg-muted p-2 rounded-lg">
+          <code className="text-muted-foreground bg-muted text-sm whitespace-pre-wrap">
+            {`
+"use client"
+
+import * as React from "react"
+import { HexColorPicker } from "react-colorful"
+import { cn } from "@/lib/utils"
+
+export type ColorPickerProps = React.ComponentProps<typeof HexColorPicker>
+
+const ColorPicker = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, children, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn("w-full h-[200px] rounded-md border shadow-sm", className)}
+    {...props}
+  >
+    {children}
+  </div>
+))
+ColorPicker.displayName = "ColorPicker"
+
+const ColorPickerHex = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement> &
+  ColorPickerProps
+>(({ className, ...props }, ref) => (
+  <HexColorPicker
+    className={cn("rounded-none", className)}
+    style={{
+      width: "100%",
+      height: "85%",
+      borderRadius: "0",
+    }}
+    {...props}
+  />
+))
+ColorPickerHex.displayName = "ColorPickerHex"
+
+const ColorPickerInput = React.forwardRef<
+  HTMLInputElement,
+  React.ComponentProps<"input">
+>(({ className, type, ...props }, ref) => (
+  <input
+    type={type}
+    className={cn(
+      "flex w-full h-fit px-3 py-1 bg-transparent transition-colors uppercase text-base md:text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
+      className
+    )}
+    ref={ref}
+    {...props}
+  />
+));
+ColorPickerInput.displayName = "ColorPickerInput"
+
+export { ColorPicker, ColorPickerHex, ColorPickerInput }
+            `}
+          </code>
+        </pre>
+        <p className="text-accent-foreground">
+          Update the import paths to match your project structure.
+        </p>
+      </div>
+    </div>
+  );
+};
